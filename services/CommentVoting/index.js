18d2107c5859f68b467b16d5ff12310a9581a0af
Ethan Stafford <estafford@umass.edu>
2022-11-17 13:15:59 -0500

    Moved folders and added base files

---
+++ services/CommentVoting/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import logger from 'morgan';
+import cors from 'cors';
+import axios from 'axios';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cors());
