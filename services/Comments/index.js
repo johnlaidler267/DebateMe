450babf00f26594f6a54d74fa077544ba271b09e
Ethan Stafford <estafford@umass.edu>
2022-11-28 13:32:57 -0500

    Added API outlines for my services

--- services/Comments/index.js
+++ services/Comments/index.js
@@ -9,6 +9,32 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(cors());
 
+class Comment{
+  userID
+  parentID
+  commentID
+  content
+  postID //for getting all comments 
+  //parentType? Is this neccessary? 
+}
+app.post("/comments/create"), (req,res) => {
+  //get params 
+  //add to database 
+  //send response
+}
+
+app.get("/comments/get"), (req, res) => {
+  //doc says get by CommentID, probably should be by postID
+  //get postID
+  //get all comments with same postID from database
+  //send response
+}
+app.post("/events", (req, res) => {
+  console.log(req.body.type);
+  res.send({});
+});
+
+
 app.listen(4001, () => {
     console.log('Listening on 4001');
   });
