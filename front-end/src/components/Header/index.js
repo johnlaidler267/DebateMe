3106551c74d3700aed3c9c9ac14b9322d12cd2ba
ieongch0106 <jieong.dev@gmail.com>
2022-12-03 19:40:38 -0500

    merge search bar into header

--- front-end/src/components/Header/index.js
+++ front-end/src/components/Header/index.js
@@ -1,4 +1,5 @@
-import SearchBar from "../SearchBar";
+import SearchIcon from '@mui/icons-material/Search';
+import { Button, Form } from 'react-bootstrap';
 import { HeaderText, Header, TM } from "./TitleElements";
 
 // make a title for top of the web page
@@ -7,7 +8,27 @@ const Title = () => {
         <Header>
             <HeaderText>
                 DebateMe<TM>TM</TM>
-                <SearchBar placeholder="Search for topics..." />
+                <Form className='d-flex justify-content-center align-items-center w-50'>
+                    <Form.Group controlId="formElectionName">
+                        <Form.Control type="text" placeholder="Enter election name" style={{ 
+                            width: "24vw",
+                            height: 45,
+                            borderTopLeftRadius: "1.25rem",
+                            borderBottomLeftRadius: "1.25rem",
+                            borderTopRightRadius: 0,
+                            borderBottomRightRadius: 0
+                        }}/>
+                    </Form.Group>
+                    <Button variant="secondary" type="submit" style={{
+                        height: 45,
+                        borderTopLeftRadius: 0,
+                        borderBottomLeftRadius: 0,
+                        borderTopRightRadius: "1.25rem",
+                        borderBottomRightRadius: "1.25rem"
+                    }}>
+                        <SearchIcon />
+                    </Button>
+                </Form>
             </HeaderText>
         </Header>
     );
