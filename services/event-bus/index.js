c6a181c29f657e993e83f9e96f79d626375e50c7
ieongch0106 <jieong.dev@gmail.com>
2022-12-08 15:05:43 -0500

    event bus is working

--- services/event-bus/index.js
+++ services/event-bus/index.js
@@ -67,60 +67,60 @@ app.post('/subscribe', (req, res) => {
 });
 
 app.post('/events', async (req, res) => {
-  const event = req.query;
+  const event = req.body;
   //make subscriptable here, need to check type of incoming event and send it only to services that care about that event. 
-  let eventType = event.type; //make sure type is the name of the array
+  let eventType = event.type; //make sure type is the name of the 
   if(eventType = "commentCreated"){
-    for(let i = 0; i< commentCreated.length; i++){
+    for(let i = 0; i< commentCreatedPorts.length; i++){
       await axios.post(`http:/${commentCreatedNames}:${commentCreatedPorts[i]}/events`, event).catch((err) => {
         console.log(err.message)
       });
     }
   }
   else if (eventType = "moderated"){
-    for(let i = 0; i< moderated.length; i++){
+    for(let i = 0; i< moderatedPorts.length; i++){
       await axios.post(`http://${moderatedNames[i]}:${moderatedPorts[i]}/events`, event).catch((err) => {
         console.log(err.message)
       });
   }
  }
   else if (eventType = "commentVoted"){
-    for(let i = 0; i< commentVoted.length; i++){
+    for(let i = 0; i< commentVotedPorts.length; i++){
       await axios.post(`http://${commentVotedNames[i]}:${commentVotedPorts[i]}/events`, event).catch((err) => {
         console.log(err.message)
       });
     }
   }
   else if (eventType = "voteCreated"){
-    for(let i = 0; i< voteCreated.length; i++){
+    for(let i = 0; i< voteCreatedPorts.length; i++){
       await axios.post(`http://${voteCreatedNames[i]}:${voteCreatedPorts[i]}/events`, event).catch((err) => {
         console.log(err.message)
       });
     }
   }
   else if (eventType = "postCreated"){
-    for(let i = 0; i< postCreated.length; i++){
+    for(let i = 0; i< postCreatedPorts.length; i++){
       await axios.post(`http://${postCreatedNames[i]}:${postCreatedPorts[i]}/events`, event).catch((err) => {
         console.log(err.message)
       });
     }
   }
   else if (eventType = "postUpdated"){
-    for(let i = 0; i< postUpdated.length; i++){
+    for(let i = 0; i< postUpdatedPorts.length; i++){
       await axios.post(`http://${postUpdatedNames[i]}:${postUpdatedPorts[i]}/events`, event).catch((err) => {
         console.log(err.message)
       });
     }
   }
   else if (eventType = "postDeleted"){
-    for(let i = 0; i< postDeleted.length; i++){
+    for(let i = 0; i< postDeletedPorts.length; i++){
       await axios.post(`http://${postDeletedNames[i]}:${postDeletedPorts[i]}/events`, event).catch((err) => {
         console.log(err.message)
       });
     }
   }
   else if (eventType = "userDataRequest"){
-    for(let i = 0; i< userDataRequest.length; i++){
+    for(let i = 0; i< userDataRequestPorts.length; i++){
       await axios.post(`http://${userDataRequestNames[i]}:${userDataRequestPorts[i]}/events`, event).catch((err) => {
         console.log(err.message)
       });
