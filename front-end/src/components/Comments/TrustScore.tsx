723c201492c7da5b831d106005d2aba138184f95
John Laidler <jlaidler@umass.edu>
2022-12-12 13:37:11 -0500

    Updated front-end

---
+++ front-end/src/components/Comments/TrustScore.tsx
@@ -0,0 +1,47 @@
+import react, { useEffect, useState } from "react";
+import Badge from "react-bootstrap/Badge";
+import axios from "axios";
+import { LegendToggle } from "@mui/icons-material";
+
+//sets the trust score of the user
+const setTrustScore = (trustScore: number) => {
+  setTrustScore(trustScore);
+};
+type Props = {
+  userId: string;
+};
+
+// Gets the trust score of the user from the backend and sets the trust score
+const TrustScore = ({ userId }: Props) => {
+  let [score, setScore] = useState(0);
+
+  const getTrustScore = async () => {
+    score = await axios.get(
+      `http://localhost:4002/getTrustScore?userId=${userId}`
+    );
+    setScore(score);
+  };
+
+  // Runs the getTrustScore function when the component is mounted
+  useEffect(() => {
+    getTrustScore();
+  }, []);
+
+  // Renders the trust score of the user
+  return (
+    <div
+      style={{
+        alignItems: "center",
+        padding: "5px",
+        marginBottom: "5px",
+        marginLeft: "3px",
+      }}
+    >
+      <Badge bg="secondary" className=" m-0 mt-2 mb-2">
+        {score}
+      </Badge>
+    </div>
+  );
+};
+
+export default TrustScore;
